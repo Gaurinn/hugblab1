132091f9128796397f8da87b6871961d75ab4cfb
Gaurinn <andri.saem@gmail.com>
2018-10-11 22:24:12 +0000

    committing

---
+++ greeting.js
@@ -0,0 +1,13 @@
+//greeting.js
+/*
+function greeting() {
+	console.log("Hello, World!");
+}
+
+greeting();
+*/
+function greeting(name) {
+	return "Hello, " + name + "!";
+}
+
+module.exports = greeting;
